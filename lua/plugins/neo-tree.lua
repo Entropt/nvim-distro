c2dd0563e83f8c9ad0ce7e5445e2bc2c743844d1
Entropt <tuan.mh.do@gmail.com>
2026-02-10 10:40:14 +0000

    add line number column in file explorer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.number = true
+          vim.opt_local.relativenumber = true
+        end,
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = true, -- 👈 show hidden files
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,5 +4,44 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    opts = function(_, opts)
+      -- Hook into Telescope to enable line numbers on the results window.
+      -- Telescope creates windows with style="minimal" which disables number.
+      -- We wrap attach_mappings to re-enable number after the picker is mounted.
+      local defaults = opts.defaults or {}
+      local original_attach = defaults.attach_mappings
+
+      defaults.attach_mappings = function(prompt_bufnr, map)
+        -- Find the picker and enable line numbers on the results window
+        vim.schedule(function()
+          local ok, action_state = pcall(require, "telescope.actions.state")
+          if not ok then return end
+          local picker = action_state.get_current_picker(prompt_bufnr)
+          if picker and picker.results_win and vim.api.nvim_win_is_valid(picker.results_win) then
+            vim.wo[picker.results_win].number = true
+            vim.wo[picker.results_win].relativenumber = true
+          end
+        end)
+
+        -- Call original attach_mappings if it exists
+        if original_attach then
+          return original_attach(prompt_bufnr, map)
+        end
+        return true
+      end
+
+      opts.defaults = defaults
+      return opts
+    end,
+    init = function()
+      -- Enable line numbers in Telescope preview windows
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TelescopePreviewerLoaded",
+        callback = function()
+          vim.opt_local.number = true
+          vim.opt_local.relativenumber = true
+        end,
+      })
+    end,
   },
 }
