1d4ddbde219e3286229502d9bcef357e64cff2a5
Entropt <tuan.mh.do@gmail.com>
2026-02-11 07:25:07 +0000

    fix ui - mismatch color

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,39 +1,16 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
-
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
-    },
-    -- Icons can be configured throughout the interface
-    icons = {
-      -- configure the loading of the lsp in the status line
-      LSPLoading1 = "⠋",
-      LSPLoading2 = "⠙",
-      LSPLoading3 = "⠹",
-      LSPLoading4 = "⠸",
-      LSPLoading5 = "⠼",
-      LSPLoading6 = "⠴",
-      LSPLoading7 = "⠦",
-      LSPLoading8 = "⠧",
-      LSPLoading9 = "⠇",
-      LSPLoading10 = "⠏",
+    status = {
+      colors = function(colors)
+        -- Match tabline sidebar padding to Neo-tree's background
+        -- so there's no visible gap above the source selector.
+        local NeoTreeNormal = require("astroui").get_hlgroup("NeoTreeNormal", { bg = colors.tabline_bg })
+        colors.tabline_bg = NeoTreeNormal.bg
+        return colors
+      end,
     },
   },
 }
