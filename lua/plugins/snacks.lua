7d70c50d3142e65152bae42e9a0dd4fddd258e00
Entropt <tuan.mh.do@gmail.com>
2026-02-11 02:26:33 +0000

    update search functionality UI

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          -- Enable line numbers in the results/list window
+          list = {
+            wo = {
+              number = true,
+              relativenumber = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
