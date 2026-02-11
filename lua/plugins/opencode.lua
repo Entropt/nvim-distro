f854e81c8e7b3389bb8dfc5e90e7b297b5676b55
Entropt <tuan.mh.do@gmail.com>
2026-02-11 03:47:52 +0000

    Add plugin to interact with OpenCode

---
+++ lua/plugins/opencode.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "nickjvandyke/opencode.nvim",
+    dependencies = {
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Don't spawn a new opencode terminal; connect to the existing session
+        provider = {
+          enabled = false,
+        },
+      }
+
+      -- Required for opts.events.reload
+      vim.o.autoread = true
+
+      vim.keymap.set(
+        { "n", "x" },
+        "<C-a>",
+        function() require("opencode").ask("@this: ", { submit = true }) end,
+        { desc = "Ask opencode" }
+      )
+      vim.keymap.set(
+        { "n", "x" },
+        "<C-x>",
+        function() require("opencode").select() end,
+        { desc = "Execute opencode action" }
+      )
+
+      vim.keymap.set(
+        { "n", "x" },
+        "go",
+        function() return require("opencode").operator "@this " end,
+        { desc = "Add range to opencode", expr = true }
+      )
+      vim.keymap.set(
+        "n",
+        "goo",
+        function() return require("opencode").operator "@this " .. "_" end,
+        { desc = "Add line to opencode", expr = true }
+      )
+
+      vim.keymap.set(
+        "n",
+        "<S-C-u>",
+        function() require("opencode").command "session.half.page.up" end,
+        { desc = "Scroll opencode up" }
+      )
+      vim.keymap.set(
+        "n",
+        "<S-C-d>",
+        function() require("opencode").command "session.half.page.down" end,
+        { desc = "Scroll opencode down" }
+      )
+
+      -- Remap increment/decrement since <C-a> and <C-x> are taken
+      vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+      vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+    end,
+  },
+}
